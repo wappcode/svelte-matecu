ed23e8406a92f1eb50c75594366f857779d2b8fe
Jesus Abarca <jesus.abarca.g@gmail.com>
2023-09-22 15:47:50 -0600

    Configuración inicial

---
+++ src/lib/components/MatecuTopbarLayout.svelte
@@ -0,0 +1,3 @@
+<script lang="ts"></script>
+
+<h1>Hola mundo Matecu Topbar Layout</h1>
---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// Reexport your entry components here
+export { default as MatecuTopbarLayout } from '$lib/components/MatecuTopbarLayout.svelte';
